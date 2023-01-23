77320ab7f936a2f35cf77061d6948fe854636cf7
olivyou2 <olivyou2@gmail.com>
2023-01-24 01:36:19 +0900

    fix: report controller variable name modified

--- src/controller/report.js
+++ src/controller/report.js
@@ -7,46 +7,46 @@ const userService = require("../service/user");
 const reportService = require("../service/report");
 
 /**
- * 
- * @param {Request} req 
- * @param {Response} res 
+ *
+ * @param {Request} req
+ * @param {Response} res
  */
 const getReport = async (req, res) => {
-    const { itemId } = req.params;
-    const { userId } = res.locals.user;
+  const { itemId } = req.params;
+  const { userId } = res.locals.user;
 
-    const report =  await reportService.getReport({itemId, userId})
-    res.json(report)
-}
+  const report = await reportService.getReport({ itemId, userId });
+  res.json(report);
+};
 
 /**
- * 
- * @param {Request} req 
- * @param {Response} res 
+ *
+ * @param {Request} req
+ * @param {Response} res
  */
 const addReport = async (req, res) => {
-    const { itemId } = req.params;
-    const { userId } = res.locals.user;
+  const { itemId } = req.params;
+  const { id: userId } = res.locals.user;
 
-    await reportService.addReport({itemId, userId})
-    res.status(httpStatus.NO_CONTENT).send()
-}
+  await reportService.addReport({ itemId, userId });
+  res.status(httpStatus.NO_CONTENT).send();
+};
 
 /**
- * 
- * @param {Request} req 
- * @param {Response} res 
+ *
+ * @param {Request} req
+ * @param {Response} res
  */
 const removeReport = async (req, res) => {
-    const { itemId } = req.params;
-    const { userId } = res.locals.user;
+  const { itemId } = req.params;
+  const { userId } = res.locals.user;
 
-    await reportService.removeReport({itemId, userId})
-    res.status(httpStatus.NO_CONTENT).send()
-}
+  await reportService.removeReport({ itemId, userId });
+  res.status(httpStatus.NO_CONTENT).send();
+};
 
 module.exports = {
-    getReport: asyncWrapper(getReport),
-    addReport: asyncWrapper(addReport),
-    removeReport: asyncWrapper(removeReport)
+  getReport: asyncWrapper(getReport),
+  addReport: asyncWrapper(addReport),
+  removeReport: asyncWrapper(removeReport),
 };
